9d1dc8892a40bf403d3a0a1b1c8576f6d212c74f
Phillip Rower <prower@examsoft.com>
2021-12-06 12:56:54 -0700

    add example using sdk 2

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,18 +1,19 @@
 package helloworld;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import layer.CommonCode;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import layer.CommonCode;
 
 /**
  * Handler for requests to Lambda function.
@@ -23,7 +24,10 @@ public class App implements RequestHandler<Object, Object> {
         // Calling code from layer
         final LambdaLogger logger = context.getLogger();
         CommonCode.doSomethingOnLayer(logger, "fromLambdaFunction");
-
+        logger.log("Log all urls:");
+        SqsClient sqsClient = SqsClient.builder().build();
+        ListQueuesResponse listQueuesResponse = sqsClient.listQueues();
+        listQueuesResponse.queueUrls().stream().forEach(urls ->logger.log(urls));
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
--- sample-layer/src/main/java/layer/CommonCode.java
+++ sample-layer/src/main/java/layer/CommonCode.java
@@ -1,10 +1,19 @@
 package layer;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 
 public class CommonCode {
 
     public static void doSomethingOnLayer(final LambdaLogger logger, final String s) {
         logger.log("Doing something on layer" + s);
+        S3Client s3Client = S3Client.builder().build();
+
+        logger.log("list buckets: ");
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3Client.listBuckets(listBucketsRequest);
+        listBucketsResponse.buckets().stream().forEach(x -> logger.log(x.name()));
     }
 }
